46deddf563d4a56d9a9bbc4d9a7ae79da9182f36
Kateryna Hladka <pi60_gkv@student.ztu.udu.ua>
2022-11-09 14:59:43 +0200

    Changed the message in the first three lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Перший рядок");
+Console.WriteLine("Другий рядок");
+Console.WriteLine("Третiй рядок");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
